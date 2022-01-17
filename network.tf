f6dac4f3c1ffcc83d1a2470c687a93b41556252f
Fernando Matarese <nandomatarese@gmail.com>
2022-01-17 22:58:43 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/example-org-c1e9ac/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "${var.prefix}-workshop"
 }
